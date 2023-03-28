e79bd09dfb1c1e539860e71ab37f6e090ba3c4d7
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-03-27 18:22:11 -0600

    yea!

---
+++ problems/add-two-numbers.js
@@ -0,0 +1,40 @@
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+//  */
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+//  var addTwoNumbers = function(l1, l2) {
+    
+//     var arr = []
+//     var string1 = ''
+//     var string2 = ''
+    
+//     for (var i = 0; i < l1.length; i++) {
+        
+//         string1 + String(l1[i])
+//         console.log(string1)
+
+//     }
+
+//     for (var i = 0; i < l2.length; i++) {
+        
+//         string2 + String(l2[i])
+//         console.log(string2)
+
+//     }
+    
+//     return Number(string1) + Number(string2)
+    
+// };
+
+// console.log(addTwoNumbers([2,4,3], [5,6,4]))
+
+// this dont work
+// TODO: make this work
